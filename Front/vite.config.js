bb78d63e033bb677975d7cf3244410df64623e30
Sandhish <sandhish19@gmail.com>
2024-10-20 22:32:26 +0530

    host updates

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/analytics',
+    ],
+  },
   build: {
     rollupOptions: {
       external: [

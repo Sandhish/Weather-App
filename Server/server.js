62f69f9313d403100231953900fdbfa45a9b9715
Sandhish <sandhish19@gmail.com>
2024-10-23 20:10:02 +0530

    hosting changes

--- Server/server.js
+++ Server/server.js
@@ -162,9 +162,9 @@ const fetchUsersAndCheckWeather = async () => {
 
 setInterval(() => {
     fetchUsersAndCheckWeather();
-},  10000);
+},  5 * 60 * 60 * 1000);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
